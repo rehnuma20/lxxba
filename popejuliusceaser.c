99af242b13901cd2c4cd80c4f16d56fb8007e125
laibarehnuma <rehnumarashid2004@gmail.com>
2025-08-16 09:56:56 +0530

    Add files via upload

---
+++ popejuliusceaser.c
@@ -0,0 +1,10 @@
+# include<stdio.h>
+
+/*
+so i actually like to know abt julius ceaser and hitler 
+both were gays
+*/
+int main(){
+    printf("hitler wasnt that creative at allin his work tho, very mid");
+    return 0;
+}
